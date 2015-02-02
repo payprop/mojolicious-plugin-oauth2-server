470ce33930e82e5421cf53d00ab43520164fd49d
Lee Johnson <lee@givengain.ch>
2015-02-02 16:49:38 +0100

    this is almost working...
    in the sense that it will work in a single threaded single instance
    app but probably not in anything more, and certainly not between
    restarts; but the shell is there to allow the internal methods to
    be overridden with callbacks to make it more robust
    
    currently one test failing which relates to when the access token
    is revoked due to the auth code being used more than once - this
    is when the refresh token is used, since the auth code references
    the original access token, so need to update the auth code to ref
    the new token when the refresh token is used

--- lib/Mojolicious/Plugin/OAuth2/Server.pm
+++ lib/Mojolicious/Plugin/OAuth2/Server.pm
@@ -21,12 +21,13 @@ use base qw/ Mojolicious::Plugin /;
 
 use Mojo::URL;
 use Time::HiRes qw/ gettimeofday /;
-use MIME::Base64 qw/ encode_base64 /;
+use MIME::Base64 qw/ encode_base64 decode_base64 /;
 
 our $VERSION = '0.01';
 
 my %CLIENTS;
 my %AUTH_CODES;
+my %ACCESS_TOKENS;
 
 =head1 METHODS
 
@@ -47,12 +48,16 @@ sub register {
 
   %CLIENTS = %{ $config->{clients} // {} };
 
-  $app->routes->any(
-    $auth_route => sub { _authorization_request( $app,$config,@_ ) }
+  $app->routes->post(
+    $auth_route => sub { _authorization_request( $app,$config,@_ ) },
   );
 
-  $app->routes->any(
-    $atoken_route => sub {},
+  $app->routes->post(
+    $atoken_route => sub { _access_token_request( $app,$config,@_ ) },
+  );
+
+  $app->helper(
+    oauth => sub { _verify_access_token_and_scope( $app,$config,@_ ) },
   );
 }
 
@@ -83,38 +88,113 @@ sub _authorization_request {
     return;
   }
 
-  $url = Mojo::URL->new( $url );
+  my $uri = Mojo::URL->new( $url );
 
-  my $code = $config->{verify_client} // \&_verify_client;
+  my $sub            = $config->{verify_client} // \&_verify_client;
+  my ( $res,$error ) = $sub->( $self,$c_id,$c_secret,\@scopes );
 
-  if ( my $res = $code->( $self,$c_id,$c_secret,\@scopes ) ) {
+  if ( $res ) {
 
-    if ( $res eq '1' ) {
+    my ( $auth_code,$expires_at ) = _generate_authorization_code( $c_id );
 
-      my ( $auth_code,$expires_at ) = _generate_authorization_code( $c_id );
-
-      if ( $code = $config->{store_auth_code} ) {
-        $code->( $auth_code,$c_id,$c_secret,$expires_at );
-      } else {
-        _store_auth_code( $auth_code,$c_id,$c_secret,$expires_at );
-      }
-
-      $url->query->append( code  => $auth_code );
-
+    if ( $sub = $config->{store_auth_code} ) {
+      $sub->( $auth_code,$c_id,$c_secret,$expires_at,$url,@scopes );
     } else {
-      $url->query->append( error  => $res );
+      _store_auth_code( $auth_code,$c_id,$expires_at,$url,@scopes );
     }
+
+    $uri->query->append( code  => $auth_code );
+
+  } elsif ( $error ) {
+    $uri->query->append( error => $error );
   } else {
     # callback has not returned anything, assume server error
-    $url->query->append(
+    $uri->query->append(
       error             => 'server_error',
       error_description => 'call to verify_client returned unexpected value',
     );
   }
 
-  $url->query->append( state => $state ) if defined( $state );
+  $uri->query->append( state => $state ) if defined( $state );
 
-  $self->redirect_to( $url );
+  $self->redirect_to( $uri );
+}
+
+sub _access_token_request {
+  my ( $app,$config,$self ) = @_;
+
+  my ( $grant_type,$auth_code,$url,$refresh_token ) = map { $self->param( $_ ) }
+    qw/ grant_type code redirect_uri refresh_token /;
+
+  if (
+    ! defined( $grant_type )
+    or ( $grant_type ne 'authorization_code' and $grant_type ne 'refresh_token' )
+    or ( $grant_type eq 'authorization_code' and ! defined( $auth_code ) )
+    or ( $grant_type eq 'authorization_code' and ! defined( $url ) )
+  ) {
+    $self->render(
+      status => 400,
+      json   => {
+        error             => 'invalid_request',
+        error_description => 'the request was missing one of: grant_type, '
+          . 'code, redirect_uri;'
+          . 'or grant_type did not equal "authorization_code" '
+          . 'or "refresh_token"',
+        error_uri         => '',
+      }
+    );
+    return;
+  }
+
+  my $json_response = {};
+  my $status        = 400;
+  my ( $c_id,$error,$scope );
+
+  if ( $grant_type eq 'refresh_token' ) {
+    $c_id = _verify_access_token_and_scope( $app,$config,$self );
+  } else {
+    my $verify_auth_code_sub
+      = $config->{verify_auth_code} // \&_verify_auth_code;
+    ( $c_id,$error,$scope ) = $verify_auth_code_sub->( $self,$auth_code,$url );
+  }
+
+  if ( $c_id ) {
+
+    my ( $access_token,$refresh_token,$expires_in )
+      = _generate_access_token( $c_id );
+
+    my $store_access_token_sub
+      = $config->{store_access_token} // \&_store_access_token;
+
+    $store_access_token_sub->(
+      $c_id,$auth_code,$access_token,$refresh_token,$expires_in,$scope
+    );
+
+    $status        = 200;
+    $json_response = {
+      access_token  => $access_token,
+      token_type    => 'bearer',
+      expires_in    => $expires_in,
+      refresh_token => $refresh_token,
+    };
+
+  } elsif ( $error ) {
+      $json_response->{error} = $error;
+  } else {
+    # callback has not returned anything, assume server error
+    $json_response = {
+      error             => 'server_error',
+      error_description => 'call to verify_auth_code returned unexpected value',
+    };
+  }
+
+  $self->res->headers->header( 'Cache-Control' => 'no-store' );
+  $self->res->headers->header( 'Pragma'        => 'no-cache' );
+
+  $self->render(
+    status => $status,
+    json   => $json_response,
+  );
 }
 
 sub _generate_authorization_code {
@@ -123,29 +203,148 @@ sub _generate_authorization_code {
   my ( $sec,$usec ) = gettimeofday;
 
   return (
-    encode_base64( join( '-',$sec,$usec,$client_id ) ),
+    encode_base64( join( '-',$sec,$usec,rand(),$client_id ),'' ),
     time + 600
   );
 }
 
-sub _store_auth_code {
-  my ( $auth_code,$client_id,$client_secret,$expires_at ) = @_;
+sub _generate_access_token {
 
-  $AUTH_CODES{$client_id}{$auth_code} = {
+  my ( $client_id ) = @_;
+
+  return (
+    ( _generate_authorization_code( $client_id ) )[0],
+    ( _generate_authorization_code( $client_id ) )[0],
+    3600,
+  );
+}
+
+sub _store_auth_code {
+  my ( $auth_code,$client_id,$expires_at,$uri,@scopes ) = @_;
+
+  $AUTH_CODES{$auth_code} = {
+    client_id     => $client_id,
     expires       => $expires_at,
-    client_secret => $client_secret,
-    checked       => 0,
+    redirect_uri  => $uri,
+    scope         => { map { $_ => 1 } @scopes },
   };
 
   return 1;
+}
+
+sub _verify_auth_code {
+  my ( $self,$auth_code,$uri ) = @_;
+
+  my ( $sec,$usec,$rand,$client_id ) = split( '-',decode_base64( $auth_code ) );
+
+  if (
+    ! exists( $AUTH_CODES{$auth_code} )
+    or $AUTH_CODES{$auth_code}{access_token}
+    or ( $uri && $AUTH_CODES{$auth_code}{redirect_uri} ne $uri )
+    or ( $AUTH_CODES{$auth_code}{expires} <= time )
+    or ( $client_id ne $AUTH_CODES{$auth_code}{client_id} )
+  ) {
+
+    if ( my $access_token = $AUTH_CODES{$auth_code}{access_token} ) {
+      # this auth code has already been used to generate an access token
+      # so we need to revoke the access token that was previously generated
+      _revoke_access_token( $access_token );
+    }
+
+    return ( 0,'invalid_grant' );
+  } else {
+    return ( 1,$client_id,$AUTH_CODES{$auth_code}{scope} );
+  }
+
+  return ( 0,'invalid_request' );
 }
 
 sub _verify_client {
   my ( $self,$client_id,$client_secret,$scopes_ref ) = @_;
 
   if ( my $client = $CLIENTS{$client_id} ) {
-    # TODO - scope checking?
-    return $client->{client_secret} eq $client_secret;
+
+    if ( $client->{client_secret} eq $client_secret ) {
+
+      foreach my $scope ( @{ $scopes_ref // [] } ) {
+
+        if ( ! exists( $CLIENTS{$client_id}{scopes}{$scope} ) ) {
+          return ( 0,'invalid_scope' );
+        } elsif ( ! $CLIENTS{$client_id}{scopes}{$scope} ) {
+          return ( 0,'access_denied' );
+        }
+      }
+
+      return ( 1 );
+
+    } else {
+      return ( 0,'access_denied' );
+    }
+  }
+
+  return ( 0,'unauthorized_client' );
+}
+
+sub _verify_access_token_and_scope {
+  my ( $app,$config,$c,@scopes ) = @_;
+
+  my $auth_header = $c->req->headers->header( 'Authorization' );
+  my ( $auth_type,$access_token ) = split( / /,$auth_header );
+
+  my $verify_access_token_sub
+    = $config->{verify_access_token} // \&_verify_access_token;
+
+  if ( $auth_type ne 'Bearer' ) {
+    return 0;
+  } else {
+    return $verify_access_token_sub->( $access_token,\@scopes );
+  }
+
+}
+
+sub _store_access_token {
+  my ( $c_id,$auth_code,$access_token,$refresh_token,$expires_in,$scope ) = @_;
+
+  $ACCESS_TOKENS{$access_token} = {
+    scope         => $scope,
+    expires       => time + $expires_in,
+    refresh_token => $refresh_token,
+    client_id     => $c_id,
+  };
+
+  $AUTH_CODES{$auth_code}{access_token} = $access_token;
+
+  return $c_id;
+}
+
+sub _revoke_access_token {
+  my ( $access_token ) = @_;
+
+  # need to revoke both the refresh token and the access token
+  delete( $ACCESS_TOKENS{ $ACCESS_TOKENS{$access_token}{refresh_token} } );
+  delete( $ACCESS_TOKENS{$access_token} );
+}
+
+sub _verify_access_token {
+  my ( $access_token,$scopes_ref ) = @_;
+
+  if ( exists( $ACCESS_TOKENS{$access_token} ) ) {
+
+    if ( $ACCESS_TOKENS{$access_token}{expires} <= time ) {
+      _revoke_access_token( $access_token );
+      return 0;
+    } elsif ( $scopes_ref ) {
+
+      foreach my $scope ( @{ $scopes_ref // [] } ) {
+        return 0 if (
+          ! exists( $ACCESS_TOKENS{$access_token}{scope}{$scope} )
+          or ! $ACCESS_TOKENS{$access_token}{scope}{$scope}
+        );
+      }
+
+    }
+
+    return $ACCESS_TOKENS{$access_token}{client_id};
   }
 
   return 0;
--- t/010_basic.t
+++ t/010_basic.t
@@ -5,61 +5,67 @@ use warnings;
 
 use Mojolicious::Lite;
 use Mojo::URL;
-use Test::Mojo;
 use Test::More;
+use Test::Deep;
+use Test::Mojo;
 
-my $t       = Test::Mojo->new;
-my $app_url = $t->ua->server->url;
-my $host    = $app_url->host;
-my $port    = $app_url->port;
+MOJO_APP: {
+  # plugin configuration
+  plugin 'OAuth2::Server' => {
+    clients              => {
+      1 => {
+        client_secret => 'boo',
+        scopes        => {
+          eat       => 1,
+          drink     => 0,
+          sleep     => 1,
+        },
+      },
+    },
+  };
 
-# plugin configuration
-plugin 'OAuth2::Server' => {
-  clients              => {
-    1 => {
-      client_secret => 'boo',
-      scopes => [ qw/ lot of nuts / ],
-    }
-  },
-  verify_client        => undef,
-  store_auth_code      => undef,
-  verify_auth_code     => undef,
-  store_client_id      => undef,
-  store_client_secret  => undef,
-  scopes_for_client_id => undef,
-  verify_access_token  => undef,
+  group {
+    # /api - must be authorized
+    under '/api' => sub {
+      my ( $c ) = @_;
+      return 1 if $c->oauth;
+      $c->render( status => 401, text => 'Unauthorized' );
+      return undef;
+    };
+
+    get '/eat' => sub { shift->render( text => "food"); };
+  };
+
+  # /sleep - must be authorized and have sleep scope
+  get '/api/sleep' => sub {
+    my ( $c ) = @_;
+    $c->oauth( 'sleep' )
+      || $c->render( status => 401, text => 'You cannot sleep' );
+
+    $c->render( text => "bed" );
+  };
 };
 
+my %valid_auth_params = (
+  client_id     => 1,
+  client_secret => 'boo',
+  response_type => 'code',
+  redirect_uri  => 'https://client/cb',
+  scope         => 'eat',
+  state         => 'queasy',
+);
+
 # now the testing begins
+my $t = Test::Mojo->new;
 note( "authorization request" );
 
-$t->get_ok( '/oauth/authorize?'
-  . join( '&',
-      'client_id=1',
-      'client_secret=boo',
-      'response_type=code',
-      'redirect_uri=https://nuts/nuts',
-      'scope=that%27s+a+lot+of+nuts',
-      'state=queasy',
-    )
-  )
-  ->status_is( 302 )
-;
-
-note( " ... authorized" );
-my $location = Mojo::URL->new( $t->tx->res->headers->location );
-note( $location );
-is( $location->path,'/nuts','redirect to right place' );
-ok( $location->query->param( 'code' ),'includes code' );
-is( $location->query->param( 'state' ),'queasy','includes state' );
-
 note( " ... not authorized (missing params)" );
-foreach my $q_string (
-  'client_secret=boo&response_type=code',
-  'client_id=1&response_type=code',
-  'client_id=1&client_secret=boo',
+foreach my $form_params (
+  { client_secret => 'boo', response_type => 'code', },
+  { client_id     => 1,     response_type => 'code', },
+  { client_id     => 1,     client_secret => 'boo', },
 ) {
-  $t->get_ok( '/oauth/authorize?',$q_string )
+  $t->post_ok( '/oauth/authorize' => form => $form_params )
     ->status_is( 400 )
     ->json_is( {
       error => 'invalid_request',
@@ -72,6 +78,129 @@ foreach my $q_string (
 }
 
 note( " ... not authorized (errors)" );
+
+foreach my $invalid_params (
+  { client_id     => 2,       error => 'unauthorized_client', },
+  { client_secret => 'eek',   error => 'access_denied', },
+  { scope         => 'cry',   error => 'invalid_scope', },
+  { scope         => 'drink', error => 'access_denied', },
+) {
+  my $expected_error = delete( $invalid_params->{error} );
+  $t->post_ok( '/oauth/authorize' => form => {
+      %valid_auth_params, %{ $invalid_params }
+    } )
+    ->status_is( 302 )
+  ;
+
+  my $location = Mojo::URL->new( $t->tx->res->headers->location );
+  is( $location->path,'/cb','redirect to right place' );
+  ok( ! $location->query->param( 'code' ),'no code' );
+  is( $location->query->param( 'error' ),$expected_error,'expected error' );
+}
+
+$t->post_ok( '/oauth/authorize' => form => \%valid_auth_params )
+  ->status_is( 302 )
+;
+
+note( " ... authorized" );
+my $location = Mojo::URL->new( $t->tx->res->headers->location );
+is( $location->path,'/cb','redirect to right place' );
+ok( my $auth_code = $location->query->param( 'code' ),'includes code' );
+is( $location->query->param( 'state' ),'queasy','includes state' );
+
+note( "access token" );
+
+my %valid_token_params = (
+  grant_type   => 'authorization_code',
+  code         => $auth_code,
+  redirect_uri => $valid_auth_params{redirect_uri},
+);
+
+$t->post_ok( '/oauth/access_token' => form => \%valid_token_params )
+  ->status_is( 200 )
+  ->header_is( 'Cache-Control' => 'no-store' )
+  ->header_is( 'Pragma'        => 'no-cache' )
+;
+
+cmp_deeply(
+  $t->tx->res->json,
+  {
+    access_token  => re( '^.{48,52}$' ),
+    token_type    => 'bearer',
+    expires_in    => '3600',
+    refresh_token => re( '^.{48,52}$' ),
+  },
+  'json_is_deeply'
+);
+
+my $access_token  = $t->tx->res->json->{access_token};
+my $refresh_token = $t->tx->res->json->{refresh_token};
+
+note( "don't use access token to access route" );
+$t->get_ok('/api/eat')->status_is( 401 );
+$t->get_ok('/api/sleep')->status_is( 401 );
+
+note( "use access token to access route" );
+
+$t->ua->on(start => sub {
+  my ( $ua,$tx ) = @_;
+  $tx->req->headers->header( 'Authorization' => "Bearer $access_token" );
+});
+
+$t->get_ok('/api/eat')->status_is( 200 );
+$t->get_ok('/api/sleep')->status_is( 401 );
+
+note( "refresh access token" );
+
+my %valid_refresh_token_params = (
+  grant_type    => 'refresh_token',
+  refresh_token => $refresh_token,
+  scope         => 'eat',
+);
+
+$t->post_ok( '/oauth/access_token' => form => \%valid_refresh_token_params )
+  ->status_is( 200 )
+  ->header_is( 'Cache-Control' => 'no-store' )
+  ->header_is( 'Pragma'        => 'no-cache' )
+;
+
+cmp_deeply(
+  $t->tx->res->json,
+  {
+    access_token  => re( '^.{48,52}$' ),
+    token_type    => 'bearer',
+    expires_in    => '3600',
+    refresh_token => re( '^.{48,52}$' ),
+  },
+  'json_is_deeply'
+);
+
+isnt( $t->tx->res->json->{access_token},$access_token,'new access_token' );
+isnt( $t->tx->res->json->{refresh_token},$refresh_token,'new refresh_token' );
+
+$access_token  = $t->tx->res->json->{access_token};
+$refresh_token = $t->tx->res->json->{refresh_token};
+
+$t->ua->on(start => sub {
+  my ( $ua,$tx ) = @_;
+  $tx->req->headers->header( 'Authorization' => "Bearer $access_token" );
+});
+
+$access_token  = $t->tx->res->json->{access_token};
+$refresh_token = $t->tx->res->json->{refresh_token};
+
+$t->get_ok('/api/eat')->status_is( 200 );
+$t->get_ok('/api/sleep')->status_is( 401 );
+
+note( "access token (2nd time with same auth code fails)" );
+
+$t->post_ok( '/oauth/access_token' => form => \%valid_token_params )
+  ->status_is( 400 )
+;
+
+note( " ... access revoked" );
+$t->get_ok('/api/eat')->status_is( 401 );
+$t->get_ok('/api/sleep')->status_is( 401 );
 
 done_testing();
 
