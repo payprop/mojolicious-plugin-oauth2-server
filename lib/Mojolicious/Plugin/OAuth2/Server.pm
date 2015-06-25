faf870342c84027f73aeeba8099988e92f453d08
Lee Johnson <lee@givengain.ch>
2015-06-25 15:31:13 +0200

    add Try::Tiny to Makefile.PL
    not core, hence...

--- lib/Mojolicious/Plugin/OAuth2/Server.pm
+++ lib/Mojolicious/Plugin/OAuth2/Server.pm
@@ -11,7 +11,7 @@ Authorization Server / Resource Server with Mojolicious
 
 =head1 VERSION
 
-0.21
+0.22
 
 =head1 SYNOPSIS
 
@@ -218,7 +218,7 @@ use Carp qw/ croak /;
 use Crypt::PRNG qw/ random_string /;
 use Try::Tiny;
 
-our $VERSION = '0.21';
+our $VERSION = '0.22';
 
 my %CLIENTS;
 my %AUTH_CODES;
