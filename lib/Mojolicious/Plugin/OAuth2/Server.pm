267378028cdd78fbd8598241907ff2571c045ece
Lee Johnson <lee@givengain.ch>
2017-03-06 15:29:47 +0100

    bump VERSION and Changes for CPAN release

--- lib/Mojolicious/Plugin/OAuth2/Server.pm
+++ lib/Mojolicious/Plugin/OAuth2/Server.pm
@@ -11,7 +11,7 @@ Authorization Server / Resource Server with Mojolicious
 
 =head1 VERSION
 
-0.34
+0.35
 
 =head1 SYNOPSIS
 
@@ -101,7 +101,7 @@ use Mojo::Util qw/ b64_decode /;
 use Net::OAuth2::AuthorizationServer;
 use Carp qw/ croak /;
 
-our $VERSION = '0.34';
+our $VERSION = '0.35';
 
 my ( $AuthCodeGrant,$PasswordGrant,$ImplicitGrant,$ClientCredentialsGrant,$Grant );
 
